52facb915ebb3d3fd247b1d6f30f24ed2b818a85
AnhNguyen <anh.nguyen17@niteco.se>
2024-07-17 17:37:34 +0700

    Initialize service schedule job

---
+++ src/BuildingBlocks/Contracts/ScheduledJobs/IScheduledJobService.cs
@@ -0,0 +1,34 @@
+﻿using SharpCompress.Common;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.ScheduledJobs
+{
+    public interface IScheduledJobService
+    {
+        // Fire-and-Forget Jobs: Fire-and-forget jobs are executed only once and almost immediately after creation.
+        string Enqueue(Expression<Action> functionCall);
+        string Enqueue<T>(Expression<Action<T>> functionCall);
+        // Delayed Jobs: Delayed jobs are executed only once too, but not immediately, after a certain time interval.
+        string Schedule(Expression<Action> functionCall, TimeSpan delaytime);
+        string Schedule<T>(Expression<Action<T>> functionCall, TimeSpan delaytime);
+        string Schedule(Expression<Action> functionCall, DateTimeOffset futureExactTime);
+        string Schedule<T>(Expression<Action<T>> functionCall, DateTimeOffset futureExactTime);
+
+        // Continuations: Continuations are executed when its parent job has been finished.
+        string ContinueQueueWith(string parentJobId, Expression<Action> functionCall);
+
+        // Recurring Jobs: Recurring jobs fire many times on the specified CRON schedule.
+        bool Delete(string jobId);
+        bool Requeue(string jobId);
+        /* Other:
+            + Batches (PRO): Batch is a group of background jobs that is created atomically and considered as a single entity.
+            + Batch Continuations(PRO): Batch continuation is fired when all background jobs in a parent batch finished.*/
+
+    }
+}
---
+++ src/BuildingBlocks/Infrastructure/ScheduledJobs/Services/HangfireServices.cs
@@ -0,0 +1,59 @@
+﻿using Contracts.ScheduledJobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.ScheduledJobs.Services
+{
+    public class HangfireServices : IScheduledJobService
+    {
+
+        public string ContinueQueueWith(string parentJobId, Expression<Action> functionCall)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(string jobId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Enqueue(Expression<Action> functionCall)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Enqueue<T>(Expression<Action<T>> functionCall)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Requeue(string jobId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Schedule(Expression<Action> functionCall, TimeSpan delaytime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Schedule<T>(Expression<Action<T>> functionCall, TimeSpan delaytime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Schedule(Expression<Action> functionCall, DateTimeOffset futureExactTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Schedule<T>(Expression<Action<T>> functionCall, DateTimeOffset futureExactTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
